d2123631aebbb6dbf3a310e904b131347ffd0a8e
thisissepehr <a.aminian1377@gmail.com>
2022-04-03 19:27:53 +0100

    VGG added

--- models/convolutionalModels.py
+++ models/convolutionalModels.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from turtle import forward
+from grpc import insecure_channel
 import torch
 from torch import nn, softmax
 from models.mlp import BaseModel
@@ -63,7 +64,6 @@ class CNN_C(BaseModel):
         super().__init__()
         self.conv_Sequence = nn.Sequential(
             nn.Conv2d(1,32,kernel_size=(3,3), padding=(1,0)),
-            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
             
@@ -94,10 +94,53 @@ class CNN_C(BaseModel):
         return x
     
     
-    
-class AlexNet(BaseModel):
-    
-    def __init__(self):
+class VGGBlock(BaseModel):
+    def __init__(self, numConvs, inputChannels, outputChannels):
         super().__init__()
+        self.numConvs = numConvs
+        for i in range(self.numConvs):
+            self.add_module('conv{0}'.format(i), nn.Conv2d(inputChannels,
+                                                           outputChannels,
+                                                           kernel_size=3,
+                                                           padding=1))
+            inputChannels = outputChannels
+            self.add_module('relu{0}'.format(i), nn.ReLU())
+
+        self.maxPool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        
     def forward(self, x):
-        pass
+        out = x
+        for i in range(self.numConvs):
+            out = self._modules['conv{0}'.format(i)](out)
+            out = self._modules['relu{0}'.format(i)](out)  
+        out = self.maxPool(out)
+        return out
+class VGG11(BaseModel):
+    
+    def __init__(self,convArch):
+        super().__init__()
+        self.convArch = convArch
+        inChannels = 1
+        for i,(numConvs, outChannels) in enumerate(convArch):
+            self.add_module('vggBlock{0}'.format(i), VGGBlock(numConvs,
+                                                              inChannels,
+                                                              outChannels))
+            inChannels = outChannels
+        self.last = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(outChannels * 7 * 7, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096,10)
+        ) 
+        
+    def forward(self, x):
+        out = x
+        for i in range(len(self.convArch)):
+            out = self._modules['vggBlock{0}'.format(i)](out)
+        out = self.last(out)
+        return out
